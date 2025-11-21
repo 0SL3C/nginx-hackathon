35806aa20e9d6e3473255361cc33c73770ea1b12
Celso Vinicius Franco Ferreira <celsovinicius_f@hotmail.com>
2025-11-21 12:06:33 +0000

    Update Vite configuration for deployment and adjust Router basename

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/nginx-hackathon/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
